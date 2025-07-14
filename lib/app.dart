4621cad6b546b44326fa9ce304cc4b11b04b9679
guillermo.parada <guillermo.parada@pragma.com.co>
2025-07-14 16:57:42 -0500

    inital commit

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:weinds/foundations/themes/weinds_themes.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ligthThemeWeincode);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:state_managers_app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
